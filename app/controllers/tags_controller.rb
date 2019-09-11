f8bfed71ee961b825485b34a24d3725ee5744cea
Onur Kaya <onurkaya8@outlook.com>
2019-09-11 20:41:13 +0300

    index and destroy method added for tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,17 @@
 class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
-    end      
+    end
+    
+    def index
+        @tags = Tag.all 
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "TAG x-> #{@tag.name} DESTROYED!"
+
+        redirect_to action: 'index'
+    end
 end
